e453d5b6879cef5090d894973fa2a89be20d62c8
Quek Yang Sheng Tyson <quektyson@gmail.com>
2017-05-24 12:23:28 +0800

    Implement network call to fetch sample JSON from a backend

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,12 +2,28 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
 class AlbumList extends Component {
+    state = { albums: [] };
+
     render() {
+        console.log(this.state);
         return (
             <View>
                 <Text>Album List</Text>
             </View>
         );
+    }
+
+    //lifecycle methods
+    /*make some http requests here*/
+    componentWillMount() {
+        console.log('[AlbumList] componentWillMount');
+        console.log('[AlbumList] fetching album list...');
+        fetch('https://rallycoding.herokuapp.com/api/music_albums/')
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                console.log(responseJSON);
+                this.setState({ albums: responseJSON });
+            }).catch((error) => console.error(error));
     }
 }
 
